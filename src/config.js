c84fbf98f6de8b0e94b1b3b2cd03d2d916db8f8e
Frank Chen H <frank.h.chen@ericsson.com>
2017-12-22 12:21:27 +0800

    modify post dir

--- src/config.js
+++ src/config.js
@@ -9,7 +9,7 @@ module.exports = {
    * API
    */
   repo: 'chenhm/chenhm.github.com', // Which repository you stored the posts
-  path: '_posts',
+  path: 'posts',
   branch: 'master',
   disqus_shortname: 'chenhm'
 }
